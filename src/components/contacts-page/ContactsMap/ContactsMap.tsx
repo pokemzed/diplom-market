1ade0037202e3c9cb6e380ce1f6ffc681323c78b
Kirill Dubasov <cergocergo41@gmail.com>
2023-06-30 12:22:37 +0300

    redact ContactsMap.tsx

--- src/components/contacts-page/ContactsMap/ContactsMap.tsx
+++ src/components/contacts-page/ContactsMap/ContactsMap.tsx
@@ -1,31 +1,24 @@
 import React from 'react';
 import styles from "./ContactsMap.module.css";
-import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
-import {LatLngExpression} from "leaflet";
+import {YMaps, Map, Placemark} from "@pbe/react-yandex-maps";
 
 
 const ContactsMap = () => {
 
-	const position:LatLngExpression = [56.228, 43.399];
+	const position = [56.228, 43.399];
 
 	return (
-		<MapContainer
-			className={styles.ContactsMap}
-			center={position}
-			zoom={14}
-			scrollWheelZoom={false}
-		>
-			<TileLayer
-				attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-				url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-			/>
-			<Marker position={position}>
-				<Popup>
-					ТЦ Меркурий <br />
-					г.Дзержинск
-				</Popup>
-			</Marker>
-		</MapContainer>
+		<YMaps>
+			<Map
+				defaultState={{
+					center: position,
+					zoom: 15,
+				}}
+				className={styles.ContactsMap}
+			>
+				<Placemark geometry={position} />
+			</Map>
+		</YMaps>
 	);
 };
 
