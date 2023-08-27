9aa30f0abe67f286f6b6e1ed83367bd81fc365fb
Kirill Dubasov <cergocergo41@gmail.com>
2023-08-27 11:48:10 +0300

    add yandex metrika to layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Providers from "@/app/providers";
 import type {Metadata} from "next";
 import {APP_TITLE} from "@/constants/general";
+import Head from "next/head";
 
 export const metadata: Metadata = {
     title: APP_TITLE,
@@ -21,10 +22,44 @@ interface IRootLayout {
 const RootLayout = ({children}:IRootLayout) => {
   return (
       <html lang="ru">
+          <Head>
+              <meta name="yandex-verification" content="5d4090c515673279" />
+          </Head>
+
           <body>
               <Providers>
                   {children}
               </Providers>
+
+              {/*yandex metrika*/}
+              <script
+                  dangerouslySetInnerHTML={{
+                      __html: `
+                                (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                                m[i].l=1*new Date();
+                                for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+                                (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+        
+                                ym(94748705, "init", {
+                                    clickmap:true,
+                                    trackLinks:true,
+                                    accurateTrackBounce:true
+                                });
+                              `,
+                  }}
+              />
+
+              {/*yandex metrika*/}
+              <noscript>
+                  <div>
+                      <img
+                          src="https://mc.yandex.ru/watch/94748705"
+                          style={{ position: "absolute", left: -9999 }}
+                          alt=""
+                      />
+                  </div>
+              </noscript>
           </body>
       </html>
   )
